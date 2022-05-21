ca450f2c49e5f95aa145f423d67deca2499f65c1
Joni Suovuori <suovuori.joni@gmail.com>
2022-05-22 00:28:40 +0300

    fixed linting

--- cypress/integration/anecdote-test.js
+++ cypress/integration/anecdote-test.js
@@ -1,22 +1,22 @@
 describe('Anecdotes', function() {
-    it('front page can be opened', function() {
-      cy.visit('http://localhost:3000')
-      cy.contains('Anecdotes')
-      cy.contains('create new')
-    })
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Anecdotes')
+    cy.contains('create new')
   })
-  
-  describe('Anecdotes', function() {
-    it('Anecdotes are loaded', function() {
-      cy.visit('http://localhost:3000')
-      cy.contains('If it hurts, do it more often')
-    })
-  })
+})
 
-  describe('Anecdotes', function() {
-    it('Filter works', function() {
-      cy.visit('http://localhost:3000')
-      cy.get('#filterInput').type('Adding manpower to a late software project makes it later!')
-      cy.contains('Adding manpower to a late software project makes it later!')
-    })
+describe('Anecdotes', function() {
+  it('Anecdotes are loaded', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('If it hurts, do it more often')
   })
+})
+
+describe('Anecdotes', function() {
+  it('Filter works', function() {
+    cy.visit('http://localhost:3000')
+    cy.get('#filterInput').type('Adding manpower to a late software project makes it later!')
+    cy.contains('Adding manpower to a late software project makes it later!')
+  })
+})
